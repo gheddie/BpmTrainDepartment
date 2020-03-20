1bc3af9a0b28b5942229e856490fae8ae56131ad
Sts <Sts@YellowMeerkat.sternico.local>
2020-03-20 18:28:53 +0100

    123

--- src/test/java/de/gravitex/bpm/traindepartment/runner/taskmapping/TaskMapperFactory.java
+++ src/test/java/de/gravitex/bpm/traindepartment/runner/taskmapping/TaskMapperFactory.java
@@ -17,14 +17,22 @@ public class TaskMapperFactory {
 		taskMappers.put(TaskMappingType.EVAULATE_WAGGON, new EvaluateWaggonTaskMapper());
 		taskMappers.put(TaskMappingType.PROMPT_WAGGON_REPAIR, new PromptWaggonRepairTaskMapper());
 	}
+	
+	// TODO cache it!!
+	private static HashMap<TaskMappingType, HashMap<String, String>> taskMappings = new HashMap<TaskMappingType, HashMap<String,String>>();
 
 	public static String map(TaskMappingType taskMappingType, ProcessInstance processInstance, String waggonNumber,
 			ProcessEngineServices processEngine) {
-		TaskMapper taskMapper = taskMappers.get(taskMappingType);
-		return getWaggonNumberToTaskIdMapping(
-				processEngine.getTaskService().createTaskQuery().taskDefinitionKey(taskMapper.getTaskName())
-						.processInstanceId(processInstance.getId()).taskAssignee(taskMapper.getRole()).list(),
-				taskMapper.getVariableName(), processEngine).get(waggonNumber);
+		if (taskMappings.get(taskMappingType) == null) {
+			TaskMapper taskMapper = taskMappers.get(taskMappingType);
+			HashMap<String, String> waggonNumberToTaskIdMapping = getWaggonNumberToTaskIdMapping(
+					processEngine.getTaskService().createTaskQuery().taskDefinitionKey(taskMapper.getTaskName())
+							.processInstanceId(processInstance.getId()).taskAssignee(taskMapper.getRole()).list(),
+					taskMapper.getVariableName(), processEngine);
+			taskMappings.put(taskMappingType, waggonNumberToTaskIdMapping);
+		}
+		return taskMappings.get(taskMappingType).get(waggonNumber);
+
 	}
 
 	@SuppressWarnings("unchecked")
